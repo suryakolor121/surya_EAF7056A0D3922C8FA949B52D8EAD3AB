fc3c19282459ce2aca886f0a2d78626b655f707a
suryakolor121 <148835873+suryakolor121@users.noreply.github.com>
2023-10-24 08:32:27 +0530

    Add files via upload

---
+++ challenge Unit 1.1/Challenge-Unit 1.1 (2).py
@@ -0,0 +1,15 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+if num < 0:
+  print("Factorial is not defined for negative numbers.")
+elif num == 0:
+  print("The factorial of 0 is 1")
+else:
+  result = factorial(num)
+  print("The factorial of {} is {}.".format(num,result))
---
+++ challenge Unit 1.1/Challenge-Unit 1.2.py
@@ -0,0 +1,10 @@
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+  
+  print("{} is a leap year.".format(year))
+  
+else:
+  
+  print("{} is not a leap year.".format(year))
+  
